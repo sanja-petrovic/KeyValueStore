15325403c3ef738d8e1589267269cef39125cf47
sanja-petrovic <sanjica.petrovic7@gmail.com>
2023-12-24 23:44:25 +0100

    feat: implemented in python

--- python/api/main.py
+++ python/api/main.py
@@ -1,7 +1,90 @@
-def main():
-    print("Hello, world!")
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+from urllib.parse import parse_qs, urlparse
+import threading
+import time
+
+
+class KeyValueStore:
+    def __init__(self):
+        self.data = {}
+        self.lock = threading.Lock()
+
+    def put(self, key, value):
+        if not isinstance(value, (int, float, str)):
+            raise ValueError(
+                "Invalid value type. Allowed types are int, float, or str."
+            )
+        with self.lock:
+            self.data[key] = value
+
+    def get(self, key):
+        with self.lock:
+            return self.data.get(key, None)
+
+    def print(self):
+        for key, value in self.data.items():
+            print(f"Key: {key}, Value: {value}")
+
+
+store: KeyValueStore = KeyValueStore()
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        start_time = time.time()
+        get_path: str = "/get?key="
+        if self.path.startswith(get_path):
+            parsed_url = urlparse(self.path)
+            query_params = parse_qs(parsed_url.query)
+            key = query_params.get("key", [None])[0]
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+
+            value = store.get(key)
+
+            self.wfile.write(str(value).encode() if value else b"Key not found")
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Elapsed time GET: {elapsed_time * 1000000}µs")
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Page not found")
+
+    def do_PUT(self):
+        start_time = time.time()
+        put_path: str = "/put?key="
+        if self.path.startswith(put_path):
+            parsed_url = urlparse(self.path)
+            query_params = parse_qs(parsed_url.query)
+            key = query_params.get("key", [None])[0]
+            value = query_params.get("value", [None])[0]
+
+            store.put(key, value)
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+
+            self.wfile.write(b"Key-value pair added successfully")
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Elapsed time PUT: {elapsed_time * 1000000}µs")
+
+
+def main(server_class=ThreadingHTTPServer, handler_class=RequestHandler, port=8080):
+    start_time = time.time()
+    server_address = ("", port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server on port {port}")
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time starting server: {elapsed_time * 1000000}µs")
+    httpd.serve_forever()
 
 
 if __name__ == "__main__":
     main()
-
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,9 @@
-use std::{collections::HashMap, io};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::time::Duration;
-use tokio::task;
+use std::collections::HashMap;
 use std::sync::Arc;
+use std::time::Instant;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::RwLock;
-
 #[derive(Debug, Clone)]
 pub enum Value {
     String(String),
@@ -18,7 +16,9 @@ pub struct KeyValueStore<K, Value> {
     data: HashMap<K, Value>,
 }
 
-impl<K: Eq + PartialEq + std::hash::Hash + std::fmt::Debug, Value: std::fmt::Debug> KeyValueStore<K, Value> {
+impl<K: Eq + PartialEq + std::hash::Hash + std::fmt::Debug, Value: std::fmt::Debug>
+    KeyValueStore<K, Value>
+{
     pub fn new() -> KeyValueStore<K, Value> {
         KeyValueStore {
             data: HashMap::new(),
@@ -40,11 +40,16 @@ impl<K: Eq + PartialEq + std::hash::Hash + std::fmt::Debug, Value: std::fmt::Deb
     }
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
+    let start_time = Instant::now();
+    println!("{:?}", start_time);
     let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
     let store = Arc::new(RwLock::new(KeyValueStore::new()));
 
+    let end_time = Instant::now();
+    let elapsed_time = end_time.duration_since(start_time);
+    println!("Elapsed time creating server: {:?}", elapsed_time);
     while let Ok((stream, _)) = listener.accept().await {
         tokio::spawn(handle_connection(stream, Arc::clone(&store)));
     }
@@ -52,7 +57,10 @@ async fn main() {
     println!("Shutting down.");
 }
 
-async fn handle_connection(mut stream: TcpStream, mut store: Arc<RwLock<KeyValueStore<String, Value>>>) {
+async fn handle_connection(
+    mut stream: TcpStream,
+    mut store: Arc<RwLock<KeyValueStore<String, Value>>>,
+) {
     let mut buffer = [0; 1024];
     if let Err(e) = stream.read(&mut buffer).await {
         eprintln!("Error reading from stream: {}", e);
@@ -61,13 +69,16 @@ async fn handle_connection(mut stream: TcpStream, mut store: Arc<RwLock<KeyValue
 
     let get = b"GET /get?key=";
     let put = b"PUT /put?key=";
-    let request_str = std::str::from_utf8(&buffer).unwrap_or("[Invalid UTF-8]");
-    println!("{}", request_str);
     if buffer.starts_with(get) {
+        let start_time = Instant::now();
         let key_start = get.len();
-        let key_end = buffer.iter().skip(get.len()).position(|&x| x == b' ').map(|pos| pos + key_start).unwrap_or(buffer.len());
+        let key_end = buffer
+            .iter()
+            .skip(get.len())
+            .position(|&x| x == b' ')
+            .map(|pos| pos + key_start)
+            .unwrap_or(buffer.len());
         let key = std::str::from_utf8(&buffer[key_start..key_end]).unwrap_or("[Invalid UTF-8]");
-        println!("Extracted key: {}", key);
 
         {
             let guard = store.read().await;
@@ -83,19 +94,41 @@ async fn handle_connection(mut stream: TcpStream, mut store: Arc<RwLock<KeyValue
                 }
             }
         }
+        let end_time = Instant::now();
+        let elapsed_time = end_time.duration_since(start_time);
+        println!("Elapsed time GET: {:?}", elapsed_time);
     } else if buffer.starts_with(put) {
-        let first_equals_sign_position = buffer.iter().position(|&x| x == b'=').unwrap_or(buffer.len());
-        let ampersand_position = buffer.iter().skip(first_equals_sign_position + 1).position(|&x| x == b'&').map(|pos| pos + first_equals_sign_position + 1).unwrap_or(buffer.len());
+        let start_time = Instant::now();
+        let first_equals_sign_position = buffer
+            .iter()
+            .position(|&x| x == b'=')
+            .unwrap_or(buffer.len());
+        let ampersand_position = buffer
+            .iter()
+            .skip(first_equals_sign_position + 1)
+            .position(|&x| x == b'&')
+            .map(|pos| pos + first_equals_sign_position + 1)
+            .unwrap_or(buffer.len());
         let key_vec: Vec<u8> = buffer[first_equals_sign_position + 1..ampersand_position].to_vec();
 
         let key_str = String::from_utf8_lossy(&key_vec);
 
-        let second_equals_sign_position = buffer.iter().skip(first_equals_sign_position + 1).position(|&x| x == b'=').map(|pos| pos + first_equals_sign_position + 1).unwrap_or(buffer.len());
-        let second_space_position = buffer.iter().skip(second_equals_sign_position + 1).position(|&x| x == b' ').map(|pos| pos + second_equals_sign_position + 1).unwrap_or(buffer.len());
-        
-        let value_vec: Vec<u8> = buffer[second_equals_sign_position + 1..second_space_position].to_vec();
+        let second_equals_sign_position = buffer
+            .iter()
+            .skip(first_equals_sign_position + 1)
+            .position(|&x| x == b'=')
+            .map(|pos| pos + first_equals_sign_position + 1)
+            .unwrap_or(buffer.len());
+        let second_space_position = buffer
+            .iter()
+            .skip(second_equals_sign_position + 1)
+            .position(|&x| x == b' ')
+            .map(|pos| pos + second_equals_sign_position + 1)
+            .unwrap_or(buffer.len());
+
+        let value_vec: Vec<u8> =
+            buffer[second_equals_sign_position + 1..second_space_position].to_vec();
         let value_str = String::from_utf8_lossy(&value_vec);
-        println!("Extracted value: {}", value_str);
         let value = match value_str.parse::<i64>() {
             Ok(int_val) => Value::Int(int_val),
             Err(_) => match value_str.parse::<f64>() {
@@ -103,9 +136,8 @@ async fn handle_connection(mut stream: TcpStream, mut store: Arc<RwLock<KeyValue
                 Err(_) => Value::String(value_str.to_string()),
             },
         };
-        println!("{:?}", value);
 
-        {     
+        {
             let mut guard = store.write().await;
             guard.put(key_str.to_string(), value);
         }
@@ -114,6 +146,9 @@ async fn handle_connection(mut stream: TcpStream, mut store: Arc<RwLock<KeyValue
         if let Err(e) = stream.write_all(response.as_bytes()).await {
             eprintln!("Error writing to stream: {}", e);
         }
+        let end_time = Instant::now();
+        let elapsed_time = end_time.duration_since(start_time);
+        println!("Elapsed time PUT: {:?}", elapsed_time);
     } else {
         let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
         if let Err(e) = stream.write_all(response.as_bytes()).await {
